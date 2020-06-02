eb5d99f105b6da7df54ee6f66d5bfab2863ca490
kylest0kes <kylestokes1551@gmail.com>
2020-06-02 18:07:13 -0400

    added prompts to select what team member to add

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const buildManager = require("./questions/manager.js");
-const buildEngineer = require("./questions/engineer.js");
-const buildIntern = require("./questions/intern.js");
+const buildManager = require("./questions/manager");
+const buildEngineer = require("./questions/engineer");
+const buildIntern = require("./questions/intern");
+const addMember = require("./questions/addmember");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
---
+++ questions/addmember.js
@@ -0,0 +1,10 @@
+const addMember = [
+    {
+        type: "list", 
+        message: "What type of team member would you like to add?",
+        choices: ["Engineer", "Intern", "I don't want to add anymore team members."],
+        name: "addMember"
+      },
+]
+
+module.exports = addMember;
